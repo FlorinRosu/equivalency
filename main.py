b4835e75b5359f1f61cb14b89e24d4abf41ec195
Florin Rosu <florin.rosu@e-uvt.ro>
2024-09-21 02:30:39 +0300

    more english lectures equivalence

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from doEquivalency import getNotEquivalent
 from doEquivalency import getEquivelancy
 from parseTemplate import parseTemplate
@@ -6,28 +7,33 @@ from parseStudent import parseFromPdf
 from studentData import Lecture
 
 #student = parseFromPdf("")
-student = parseFromXls("inputFile/SOLNOC.xls")
-print(student)
+listOfStudents = [fileName for fileName in os.listdir("./inputFile/") if ".xls" in fileName]
+print (listOfStudents)
 
-parser = parseTemplate("inputFile/5. Reinmatriculare.docx")
-lecturesToBeEqui = parser.getListOfLectures() 
-
-print("STUDENT LECTURE")
-for l in student._listLecture:
-    print(l)
-print("TO EQ")
-for l in lecturesToBeEqui:
-    print(l)
-
-equivalencyPairs = getEquivelancy(student, lecturesToBeEqui) #list of pairs of lectures
-
-notEquivalent = getNotEquivalent(student, equivalencyPairs) #list of lectures promoted but not equivaled
-
-print("WHAT HAVE EQ")
-for eq in equivalencyPairs:
-    print(eq[0], "<=>", eq[1])
-print("WHAT DOESNT HAVE")
-for noteq in notEquivalent:
-    print(noteq)
-
-parser.writeDocument(student, equivalencyPairs, notEquivalent)
+listOfStudents = ["BRAT.xls"]
+for studentFile in listOfStudents:
+    student = parseFromXls("inputFile/"+ studentFile)
+    print(student)
+    
+    parser = parseTemplate("inputFile/5. Reinmatriculare.docx")
+    lecturesToBeEqui = parser.getListOfLectures() 
+    
+    print("STUDENT LECTURE")
+    for l in student._listLecture:
+        print(l)
+    print("TO EQ")
+    for l in lecturesToBeEqui:
+        print(l)
+    
+    equivalencyPairs = getEquivelancy(student, lecturesToBeEqui) #list of pairs of lectures
+    
+    notEquivalent = getNotEquivalent(student, equivalencyPairs) #list of lectures promoted but not equivaled
+    
+    print("WHAT HAVE EQ")
+    for eq in equivalencyPairs:
+        print(eq[0], "<=>", eq[1])
+    print("WHAT DOESNT HAVE")
+    for noteq in notEquivalent:
+        print(noteq)
+    
+    parser.writeDocument(student, equivalencyPairs, notEquivalent)
--- parseTemplate.py
+++ parseTemplate.py
@@ -46,8 +46,74 @@ class parseTemplate():
                 self._listOfLecture.append(l)
     
     def printTableNotEquivalent(self,doc, notEquivalent):
+        table = doc.add_table(1, 7)
+        table.style = "TableGrid"
+        row = table.rows[0]
+        cells = row.cells
+        cells[0].text = "Disciplina"
+        cells[1].text = "ORE CURS"
+        cells[2].text = "ORE L/P/S"
+        cells[3].text = "Nota"
+        cells[4].text = "Credite"
+        cells[5].text = "Anul"
+        cells[6].text = "Semestrul"
+
+        for l in notEquivalent:
+            row = table.add_row()
+            cells = row.cells
+            cells[0].text = l._name
+            cells[1].text = str(l._nrHLec)
+            cells[2].text = l._nrHPrac
+            cells[3].text = str(l._grade)
+            cells[4].text = str(l._credit)
+            cells[5].text = str(l._year)
+            cells[6].text = l._sem
+            
         pass
     
+    def printTableForAYearAndGetCredits(self, table, equivalent):
+        countNr= 0 
+        credit = 0
+        for row in table.rows:
+            lectureName = row.cells[6].text
+            
+            
+            found = False
+            
+            equiFound = ""
+            for equi in equivalent:
+                if equi[1]._name == lectureName:
+                    equiFound = equi
+                    found = True
+                    break
+            if found == False:
+                #should not exist
+                print("NOT FOUND:",lectureName)
+                continue
+
+            cells = row.cells
+
+            countNr = countNr + 1
+            cells[0].text = str(countNr)
+
+            l = equiFound[0]
+            if l._name == "":
+                continue
+            cells[1].text = l._name
+            cells[2].text = l._nrHLec
+            cells[3].text = l._nrHPrac
+            cells[4].text = l._grade
+            cells[5].text = l._credit
+            cells[9].text = l._grade
+
+
+            try:
+                credit = credit + int(equiFound[1]._credit)
+            except:
+                print("NO CREDIT FOR ",equiFound[1]._name)
+            
+        return credit
+
     def writeDocument(self,student, equivalent, notEquivalent):
         doc = Document(self._fileName)
 
@@ -58,54 +124,17 @@ class parseTemplate():
         for table in doc.tables:
             if countYear >= student._year:
                 continue
+            credit = self.printTableForAYearAndGetCredits(table, equivalent)
 
-            creditList.append(0)
-            countNr= 0 
-            for row in table.rows:
-                lectureName = row.cells[6].text
-                
-                
-                found = False
-                
-                equiFound = ""
-                for equi in equivalent:
-                    if equi[1]._name == lectureName:
-                        equiFound = equi
-                        found = True
-                        break
-                if found == False:
-                    #should not exist
-                    print("NOT FOUND:",lectureName)
-                    continue
-
-                cells = row.cells
-
-                countNr = countNr + 1
-                cells[0].text = str(countNr)
-
-                l = equiFound[0]
-                if l._name == "":
-                    continue
-                cells[1].text = l._name
-                cells[2].text = l._nrHLec
-                cells[3].text = l._nrHPrac
-                cells[4].text = l._grade
-                cells[5].text = l._credit
-                cells[9].text = l._grade
-
-
-                try:
-                    creditList[-1] = creditList[-1] + int(equiFound[1]._credit)
-                except:
-                    print("NO CREDIT FOR ",equiFound[1]._name)
-                
+            creditList.append(credit)
             countYear = countYear+1
 
         #update table for not equivalent
         tableToUpdate = doc.tables[-2]
-
+        tableToUpdate.style = "TableGrid"
+        creditList.append(0)
         for eq in equivalent:
-            if eq[1]._grade == "-":
+            if eq[0]._id == "":
                 row = tableToUpdate.add_row()
                 cells = row.cells
                 l = eq[1]
@@ -114,6 +143,14 @@ class parseTemplate():
                 cells[3].text = l._sem
                 cells[4].text = l._credit
 
+                try:
+                    creditList[-1] = creditList[-1] + int(eq[1]._credit)
+                except:
+                    print("NO CREDIT FOR ",eq[1]._name)
+
+        self.printTableNotEquivalent(doc, notEquivalent)
+
+        #print text parragraphs
         countYear = 0
         for par in doc.paragraphs:
             text = par.text
@@ -124,5 +161,9 @@ class parseTemplate():
             if text.find("Anul de studiu _____")!=-1:
                 par.text = student._years[countYear] +";"+" "*30+ text[text.find(";")+1:]
                 countYear = countYear + 1
+            if text.find("Total credite ECTS echivalate:") != -1:
+                par.text = text + str(creditList[countYear-1])
+            if text.find("Nr. total de credite care urmează a fi obținute") != -1:
+                par.text = text[:-3] + str(creditList[-1])
         
-        doc.save("./inputFile/" + student._name+ ".docx")
+        doc.save("./inputFile/" + student._name[:student._name.find(" ")]+ ".docx")
